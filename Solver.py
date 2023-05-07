d4916d412931d812f532875656a73dc839bf5e1b
g-gonza <114447843+g-gonza@users.noreply.github.com>
2023-05-07 12:26:28 -0700

    Update3

--- Solver.py
+++ Solver.py
@@ -1,7 +1,14 @@
 #working to create a solver for a path problem, where we read a file and determine if it is solvable or not
 #our output will be able to be put into a SAT Solver
-#this deals with the 3-coloring problem
 import os 
+import array
+
+#lowercase letters corresponding to numbers starting with 1
+Letters = [{'a':1, 'b':2, 'c':3, 'd':4, 'e':5, 'f':6,
+           'g':7, 'h':8, 'i':9, 'j':10, 'k':11, 'l':12,
+           'm':13, 'n':14, 'o':15, 'p':16, 'q':17, 'r':18,
+           's':19, 't':20, 'u':21, 'v':22, 'w':23, 'x':24,
+           'y':25, 'z':26}]
 
 #filename = input("Enter the name of the file you want to read: ")
 filename = 'input_graph_1.txt'
@@ -28,10 +35,21 @@ for i in range(1, NumNodes+1):
         VerticeDictionary[i].append(Num)
         Num+=1
 
+
+VerticeConnections = {}
+for i in range(1, NumNodes+1):
+   
+   LetterList = Lines[i-1].split()
+   VerticeConnections[i] = LetterList
+   
+
+print(VerticeDictionary)
+print(VerticeConnections)
+
 for i in range(1, NumNodes+1):
     String = " ".join(str(x) for x in VerticeDictionary[i])
     print(String + " 0")
-    #
+    
 
 
 
