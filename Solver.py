f53f449a6fbdeebc653bfa515827ab244f3cc3f5
g-gonza <walerisdev@gmail.com>
2023-05-06 19:41:12 -0700

    Initialize

---
+++ Solver.py
@@ -0,0 +1,15 @@
+#working to create a solver for a path problem, where we read a file and determine if it is solvable or not
+#our output will be able to be put into a SAT Solver
+import os
+
+filename = input("Enter the name of the file you want to read: ")
+cwd = os.getcwd()
+os.chdir(os.path.join(cwd, 'InputFiles'))
+InputFile = open(filename, 'r')
+Lines = InputFile.readlines()
+InputFileContents = InputFile.read()
+
+#get the number of lines in the .txt file
+NumNodes = len(Lines)
+print(NumNodes)
+
